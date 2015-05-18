9ae1cc0014366f7fa232c1baee461ea5cf192a2f
dbrent-amazon <dbrent@amazon.com>
2015-05-18 11:35:54 -0700

    Add simple checkout and recurring samples.

---
+++ Python/recurring_payment/recurring_payment.py
@@ -0,0 +1,126 @@
+import json
+import random
+# Import 'Flask' for creating a flask application,
+# 'session' to store and retrieve session variables in every view
+# 'render_template' to render a HTML template with the given context variables
+# 'url_for' to get the URL corresponding to a view
+# 'redirect' to redirect to a given URL
+# 'request' to access the request object which contains the request data
+# 'flash' to display messages in the template
+from flask import Flask, session, render_template, url_for, redirect, request, flash
+
+app = Flask(__name__)
+app.secret_key = 'my_super_secret_key!'
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/cart', methods=['POST'])
+def cart():
+    session['merchant_id'] = request.form['merchant-id']
+    session['mws_access_key'] = request.form['mws-access-key']
+    session['mws_secret_key'] = request.form['mws-secret-key']
+    session['client_id'] = request.form['client-id']
+    return render_template('cart.html')
+
+
+@app.route('/set', methods=['GET'])
+def set():
+    session['access_token'] = request.args.get('access_token')
+    return render_template('set.html')
+
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+    from pay_with_amazon.client import PayWithAmazonClient
+
+    client = PayWithAmazonClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region='na',
+        currency_code='USD')
+
+    response = client.confirm_billing_agreement(
+        amazon_billing_agreement_id=session['billing_agreement_id'])
+
+    pretty = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+    return render_template('confirm.html', confirm_response=pretty)
+
+
+@app.route('/authorize', methods=['POST'])
+def authorize():
+    from pay_with_amazon.client import PayWithAmazonClient
+
+    client = PayWithAmazonClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region='na',
+        currency_code='USD')
+
+    response = client.authorize_on_billing_agreement(
+        amazon_billing_agreement_id=session['billing_agreement_id'],
+        authorization_reference_id=rand(),
+        authorization_amount='1.99',
+        seller_authorization_note='Python SDK payment authorization.',
+        transaction_timeout=0,
+        capture_now=True)
+
+    response = client.get_billing_agreement_details(
+        amazon_billing_agreement_id=session['billing_agreement_id'],
+        address_consent_token=session['access_token'])
+
+    pretty = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+    return pretty
+
+
+@app.route('/get_details', methods=['POST'])
+def get_details():
+    from pay_with_amazon.client import PayWithAmazonClient
+
+    client = PayWithAmazonClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region='na',
+        currency_code='USD')
+
+    billing_agreement_id = request.form['billingAgreementId']
+    session['billing_agreement_id'] = billing_agreement_id
+
+    response = client.set_billing_agreement_details(
+        amazon_billing_agreement_id=billing_agreement_id,
+        store_name='Pay with Amazon Python SDK')
+
+    if response.success:
+        response = client.get_billing_agreement_details(
+            amazon_billing_agreement_id=billing_agreement_id,
+            address_consent_token=session['access_token'])
+
+    pretty = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+
+    return pretty
+
+
+def rand():
+    return random.randint(0, 9999) + random.randint(0, 9999)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Python/simple_checkout/lpa-python-sdk-sample.py
@@ -0,0 +1,117 @@
+import os
+import html
+import json
+import time
+import random
+# Import 'Flask' for creating a flask application,
+# 'session' to store and retrieve session variables in every view
+# 'render_template' to render a HTML template with the given context variables
+# 'url_for' to get the URL corresponding to a view
+# 'redirect' to redirect to a given URL
+# 'request' to access the request object which contains the request data
+# 'flash' to display messages in the template
+from flask import Flask, session, render_template, url_for, redirect, request, flash
+from pay_with_amazon.client import PayWithAmazonClient
+
+app = Flask(__name__)
+app.secret_key = 'my_super_secret_key!'
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/cart', methods=['POST'])
+def cart():
+    session['merchant_id'] = request.form['merchant-id']
+    session['mws_access_key'] = request.form['mws-access-key']
+    session['mws_secret_key'] = request.form['mws-secret-key']
+    session['client_id'] = request.form['client-id']
+    return render_template('cart.html')
+
+
+@app.route('/set', methods=['GET'])
+def set():
+    session['access_token'] = request.args.get('access_token')
+    return render_template('set.html')
+
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+    from pay_with_amazon.client import PayWithAmazonClient
+
+    pretty_confirm = None
+    pretty_authorize = None
+
+    client = PayWithAmazonClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region='na',
+        currency_code='USD')
+
+    response = client.confirm_order_reference(
+        amazon_order_reference_id=session['order_reference_id'])
+
+    pretty_confirm = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+
+    if response.success:
+        response = client.authorize(
+            amazon_order_reference_id=session['order_reference_id'],
+            authorization_reference_id=rand(),
+            authorization_amount='19.95',
+            transaction_timeout=0,
+            capture_now=True)
+
+    pretty_authorize = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+
+    return render_template(
+        'confirm.html', confirm=pretty_confirm, authorize=pretty_authorize)
+
+
+@app.route('/get_details', methods=['POST'])
+def get_details():
+    from pay_with_amazon.client import PayWithAmazonClient
+
+    client = PayWithAmazonClient(
+        mws_access_key=session['mws_access_key'],
+        mws_secret_key=session['mws_secret_key'],
+        merchant_id=session['merchant_id'],
+        sandbox=True,
+        region='na',
+        currency_code='USD')
+
+    order_reference_id = request.form['orderReferenceId']
+    session['order_reference_id'] = order_reference_id
+
+    response = client.set_order_reference_details(
+        amazon_order_reference_id=order_reference_id,
+        order_total='19.95')
+
+    if response.success:
+        response = client.get_order_reference_details(
+            amazon_order_reference_id=order_reference_id,
+            address_consent_token=session['access_token'])
+
+    pretty = json.dumps(
+        json.loads(
+            response.to_json()),
+        indent=4)
+
+    return pretty
+
+
+def rand():
+    return random.randint(0, 9999) + random.randint(0, 9999)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
